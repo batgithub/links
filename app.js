715db39d464dbba9ddd79d898df4136c9ce9f3f2
Baptiste Lhopitault <baptiste.lhopitault@morning.com>
2016-11-25 20:27:41 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+function loader() {
+    $("button").each(
+        function(){
+            var file = $(this).data('file');
+            var audio = '<audio preload="auto" id="'+file +'"><source src="audio/'+ file +'.mp3"><span>'+ +'</span></audio>';
+            var dure = '<span>'+ $('#'+ file)+'</span>';
+
+            var b = $('#'+ file);
+
+            $(this).append(audio);
+        }
+
+    )
+}
+
+
+
+function playSound(id) {
+
+    var audioClick = $('#'+ id)[0];
+    var duration = (audioClick.duration)*1000;
+
+
+    audioClick.play();
+    audioClick.onplaying=function(){
+        $(audioClick).parent().addClass( "active" );
+    };
+
+    setTimeout(function(){ $(audioClick).parent().removeClass( "active" );}, duration);
+
+}
